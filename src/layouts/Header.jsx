209cd79cee085bb4964afaf41c304f50aa3db0bf
Jean-Marie <jeanmariedeschamps25@gmail.com>
2024-04-02 14:45:10 +0200

    Logo Navigation to home page

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,16 @@
 import Logo from "../components/Logo.jsx";
 import Navigation from "../components/Navigation";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <Logo />
+      <NavLink
+        to="/kasa.version2"
+        className={(nav) => (nav.isActive ? "nav-active" : "")}
+      >
+        <Logo />
+      </NavLink>
       <Navigation />
     </header>
   );
